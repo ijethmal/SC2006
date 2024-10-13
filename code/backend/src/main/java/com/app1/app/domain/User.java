a654c91f8afbf54c0839a3bd03ec139034c60233
Isha <s10205038@connect.np.edu.sg>
2024-10-13 21:09:45 +0800

    add userrepo

--- code/backend/src/main/java/com/app1/app/domain/User.java
+++ code/backend/src/main/java/com/app1/app/domain/User.java
@@ -1,8 +1,32 @@
 package com.app1.app.domain;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.UuidGenerator;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(NON_DEFAULT)
+@Table(name = "users")
+
+
 public class User {
 
     //fields
+    @Id
+    @UuidGenerator
+    @Column(name = "id", unique = true, updatable = false)
+    private String id;
 
     //constructor
 
