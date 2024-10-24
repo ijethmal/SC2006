b0542ef00b293038dc3dbbf0ec6a2663429cc3b1
Luu Thang <luuthang7803@gmail.com>
2024-10-24 09:22:29 +0800

    login, connect frontend with backend, done

--- code/frontend/src/api/UserService.js
+++ code/frontend/src/api/UserService.js
@@ -1,14 +1,14 @@
 import axios from "axios";
 
-const API_URL = 'http://localhost:8080/users';
+const API_URL = "http://localhost:8080/users/login";
 
 export async function getUsers() {
-  return axios.get(API_URL);
+    return axios.get(API_URL);
 }
 
 export async function login(email, password) {
-  return await axios.get(API_URL, {
-    email,
-    password
-  });
+    return await axios.post(API_URL, {
+        email,
+        password,
+    });
 }
