ac3c48b5e086e349392b9393507ac6faec896f94
Viet Khoa Tran <vietkhoa2801@gmail.com>
2024-10-26 12:55:40 +0800

    interest groups (add and remove members) LETS GO

---
+++ code/backend/src/main/java/com/app1/app/domain/InterestGroup.java
@@ -0,0 +1,35 @@
+package com.app1.app.domain;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import org.hibernate.annotations.UuidGenerator;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_DEFAULT)
+@Table(name = "interestgroups")
+
+public class InterestGroup {
+    @Id
+    @UuidGenerator
+    @Column(name = "id", unique = true, updatable = false)
+    private String id;
+    private String name;
+    private String activityType;
+    private String location;
+    private String createdBy;                  // Reference to the User who created the group
+    private HashMap<String, Integer> members;  // List of users who are members of the group
+    private HashMap<String, Integer> events;   // List of events in the group 
+}
