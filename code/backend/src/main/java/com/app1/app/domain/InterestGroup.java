a6f3ac0d193394594e7d8ca055803f28e7e30090
Viet Khoa Tran <vietkhoa2801@gmail.com>
2024-10-29 00:28:03 +0800

    created put and delete methods for users

--- code/backend/src/main/java/com/app1/app/domain/InterestGroup.java
+++ code/backend/src/main/java/com/app1/app/domain/InterestGroup.java
@@ -9,7 +9,6 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import java.util.ArrayList;
 import java.util.HashMap;
 import org.hibernate.annotations.UuidGenerator;
 
